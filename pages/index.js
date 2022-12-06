5d293ff707ba5e0f48d24464bed4c7e38dd7e6ce
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-12-06 19:40:10 +0100

    added about us card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Logo from '../images/logo.png'
+import AboutUs from '../images/aboutus2.jpeg'
 
 export default function Home() {
     return (
@@ -11,23 +12,37 @@ export default function Home() {
                 <meta name="keywords" content="home,food," />
             </Head>
             <section className='jumbotron' >
-                <div className='opacity'>
-                    {/* <div className=' container'> */}
-                    {/* <span></span> */}
+                <div className='opacity flex flex-col justify-around'>
                     <Image
                         src={Logo}
                         alt='logo'
                         width={200}
                         height={500}
-
                     />
                     <h1 className='text-4xl font-bold text-center'  > Degustando Italia </h1>
-                    {/* </div> */}
                 </div>
 
             </section>
-            <section>
+            <section className='about_us flex flex-wrap'>
+                <div className='about_us_card p-9'>
+                    <Image
+                        src={Logo}
+                        alt='logo'
+                        width={500}
+                        height={500}
+                    />
+                    <p className='font-light text-3xl'>La Società DEGUSTANDO ITALIA s.b. Srl si occupa di promuovere nel territorio mondiale il marchio DEGUSTANDO ITALIA ed i marchi collegati, al fine di valorizzare i prodotti e servizi del territorio.</p>
+                </div>
+                <div className='about_us_card'>
+                    <div className='about_us2_image'></div>
+                </div>
+                <div className='about_us_card'>
+
+                </div>
+                <div className='about_us_card'>
+                </div>
                 <div>
+
                 </div>
 
             </section>
