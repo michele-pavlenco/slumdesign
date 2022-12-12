edaeb847d444ae1c7ebce97fe46c1cf35dd7b86f
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-12-12 12:17:29 +0100

    Added comments

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,13 @@ import Card8 from '../images/armonia8.jpeg'
 export default function Home() {
     return (
         <div>
+            {/* Head */}
             <Head>
                 <title>Degustando Italia | Home page</title>
                 <meta name="description" content="Degustando Italia home page" />
                 <meta name="keywords" content="home,food," />
             </Head>
+            {/* Jumbotron */}
             <section className='jumbotron' >
                 <div className='opacity flex flex-col justify-around'>
                     <Image
@@ -31,7 +33,9 @@ export default function Home() {
                 </div>
 
             </section>
+            {/* Cards */}
             <section className='about_us flex flex-wrap'>
+                {/* Card 1 */}
                 <div className='about_us_card p-9'>
                     <Image
                         src={Logo}
@@ -41,9 +45,11 @@ export default function Home() {
                     />
                     <p className='font-light text-xl sm:text-4xl'>La Società DEGUSTANDO ITALIA s.b. Srl si occupa di promuovere nel territorio mondiale il marchio DEGUSTANDO ITALIA ed i marchi collegati, al fine di valorizzare i prodotti e servizi del territorio.</p>
                 </div>
+                {/* Card 2 */}
                 <div className='about_us_card'>
                     <div className='about_us2_image'></div>
                 </div>
+                {/* Card 3 */}
                 <div className='about_us_card'>
                     <div>
                         <Image
@@ -58,6 +64,7 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+                {/* Card 4 */}
                 <div className='about_us_card py-5 sm:text-2xl px-8 font-light text-base '>
                     Oggi vi portiamo nel mondo del turismo, nel mondo di chi ama l’Italia e vuole ricercare la qualità della vita per vivere una vita di qualità.
                     Il progetto Degustando Italia nasce con lo scopo di mettere al centro le persone che vivono e lavorano nel territorio, che si occupano di turismo e non solo.
@@ -72,6 +79,7 @@ export default function Home() {
                         />
                     </div>
                 </div>
+                {/* Card 5 */}
                 <div className='about_us_card  mt-20 px-9 '>
                     <h2 className='font-semibold text-4xl my-3'>
                         Il Cerchio <br />  dell' Armonia
@@ -80,14 +88,17 @@ export default function Home() {
                         Il cerchio è la figura geometrica che nella sua semplicità – non ha angoli, non ha spigoli – simboleggia la perfezione, l’armonia, la pienezza e l’unione: non ha interruzione, non ha fratture. Tanti punti, uno accanto all’altro, tutti legati tra loro ed egualmente distanti dal centro. In cerca della qualità della vita, lontano dai ritmi frenetici della città, ho intrapreso un lungo viaggio alla ricerca del Cerchio dell’Armonia e finalmente l’ho trovato…l’ho trovato tra le montagne dell’appennino Tosco Romagnolo. Ho raccolto immagini e suggestioni ricche di lussureggiante natura, di cieli azzurri, di aria pulita, di gustosi prodotti della terra, di storia e tradizione, di calorosa ospitalità, di amore e di pace. Seguimi in questo viaggio, ricco di emozioni e di naturale energia… Ti mostrerò come questi punti, tutti tra loro legati e collegati, formino Il Cerchio dell’Armonia di Degustando Italia.
                     </p>
                 </div>
+                {/* Card 6 */}
                 <div className='about_us_card pt-8'>
                     <div className='about_us5_image'></div>
                 </div>
             </section >
+            {/* Il cerchio dell armonia */}
             <section className='cerchio-dell-armonia'>
                 <div className=' container'>
                     <h2 className='text-center font-semibold text-3xl pb-5'>Il cerchio dell Armonia</h2>
                     <div className='cards flex flex-wrap '>
+                        {/* Card 1 */}
                         <div className='card  flex flex-col items-center py-3'>
                             <Image
                                 src={Card1}
@@ -95,6 +106,7 @@ export default function Home() {
                             />
                             <p className='pt-3'>Natura</p>
                         </div>
+                        {/* Card 2 */}
                         <div className='card flex flex-col items-center py-3'>
                             <Image
                                 src={Card2}
@@ -102,6 +114,7 @@ export default function Home() {
                             />
                             <p className='pt-3'>Tradizione</p>
                         </div>
+                        {/* Card 3 */}
                         <div className='card flex flex-col items-center py-3'>
                             <Image
                                 src={Card3}
@@ -109,6 +122,7 @@ export default function Home() {
                             />
                             <p className='pt-3'>Pace</p>
                         </div>
+                        {/* Card 4 */}
                         <div className='card flex flex-col items-center py-3'>
                             <Image
                                 src={Card4}
@@ -116,6 +130,7 @@ export default function Home() {
                             />
                             <p className='pt-3'>Benessere</p>
                         </div>
+                        {/* Card 5 */}
                         <div className='card flex flex-col items-center  py-3'>
                             <Image
                                 src={Card5}
@@ -123,6 +138,7 @@ export default function Home() {
                             />
                             <p className='pt-3'>Cucina</p>
                         </div>
+                        {/* Card 6 */}
                         <div className='card flex flex-col items-center  py-3'>
                             <Image
                                 src={Card6}
@@ -130,6 +146,7 @@ export default function Home() {
                             />
                             <p className='pt-3'>Calore</p>
                         </div>
+                        {/* Card 7 */}
                         <div className='card flex flex-col items-center  py-3'>
                             <Image
                                 src={Card7}
@@ -138,6 +155,7 @@ export default function Home() {
                             <p className='pt-3'>Intimità
                             </p>
                         </div>
+                        {/* Card 8 */}
                         <div className='card flex flex-col items-center py-3'>
                             <Image
                                 src={Card8}
@@ -145,14 +163,9 @@ export default function Home() {
                             />
                             <p className='pt-3'>Energia</p>
                         </div>
-
                     </div>
-
                 </div>
-
             </section>
         </div >
-
-
     );
 }
